52e9fb4ece76a015c68426abb8dd8b225ca22403
Markuson <el.marcuson@gmail.com>
2019-04-14 11:43:39 +0200

    horray until splice

--- staff/marc-uson/hooray/hooray.js
+++ staff/marc-uson/hooray/hooray.js
@@ -1,8 +1,9 @@
 'use strict';
 
 /**
- * Hooray object is a global object used to the construction of hoorays, wich are list-like objects.
- */
+* Hooray object is a global object used to the construction of hoorays, wich are list-like objects.
+*/
+
 function Hooray() {
     var first = arguments[0];
 
@@ -21,6 +22,7 @@ function Hooray() {
 *
 * @returns {number} The length of the hooray after adding the new value.
 */
+
 Hooray.prototype.push = function (value) {
     if (arguments.length > 0)
         for (var i = 0; i < arguments.length; i++)
@@ -155,9 +157,8 @@ Hooray.prototype.filter = function(callback){
 }
 
 /**
-* returns the first index of the passed array that satisfies the condition provided in the callback function
+* returns the first index of the passed hooray that satisfies the condition provided in the callback function
 *
-* @param {Array} array
 * @param {Function} callback
 */
 
@@ -171,7 +172,7 @@ Hooray.prototype.findIndex = function(callback){
 }
 
 /**
- * determines if and array contains the value determined. Return true if the value is contained or false if not
+ * determines if and hooray contains the value determined. Return true if the value is contained or false if not
  *
  * @param {any} value the value to be searched
  * @param {number} index the index which it should start the search
@@ -202,10 +203,12 @@ Hooray.prototype.indexOf = function(element, fromIndex){
 }
 
 /**
- * Returns true if the variable passed is a hooray. if not, returns false.
- *
- * @param {Variable} variable the variable to check if it's a hooray
- */
+* Returns true if the variable passed is a hooray. if not, returns false.
+*
+* @param {Variable} variable the variable to check if it's a hooray
+*
+* @returns {Boolean} true if variable is a hooray
+*/
 
 Hooray.prototype.isHooray = function(variable){
     return variable.constructor === Hooray ? true : false ;
@@ -215,6 +218,8 @@ Hooray.prototype.isHooray = function(variable){
 * Joins all the elements in the hooray using the element given as a separator. If no element is given, uses ','
 *
 * @param {any} element
+*
+* @returns {string} string containing all the elements in th hooray joined by the element passed
 */
 
 Hooray.prototype.join = function(element){
@@ -232,6 +237,8 @@ Hooray.prototype.join = function(element){
 * returns the last index in wich you can find the given element or -1 if it's not present
 *
 * @param {any} element the element to search
+*
+* @returns {Number} the las index in wich you can find the element or -1 if it's not present
 */
 
 Hooray.prototype.lastIndexOf = function(element){
@@ -247,6 +254,8 @@ Hooray.prototype.lastIndexOf = function(element){
 * creates a new hooray with the results of callback function aplied in each element of the given hooray
 *
 * @param {Function} callback the function to implement the changes wanted
+*
+* @returns {Hooray} new Hooray with the results of callback function applied in every element in the given hooray
 */
 
 Hooray.prototype.map = function(callback){
@@ -266,6 +275,8 @@ Hooray.prototype.map = function(callback){
 *
 * @param {function} callback
 * @param {number} start
+*
+* @returns {Any} the result of the operation aplied in every element in the given hooray
 */
 
 Hooray.prototype.reduceRight = function(callback, start){
@@ -290,6 +301,8 @@ Hooray.prototype.reduceRight = function(callback, start){
 *
 * @param {function} callback
 * @param {number} start
+*
+* @returns {Any} the result of the operation aplied in every element in the given hooray
 */
 
 Hooray.prototype.reduce = function(callback, start){
@@ -307,4 +320,177 @@ Hooray.prototype.reduce = function(callback, start){
         result = callback(result, this[i], i, this);
     }
     return result;
+}
+
+/**
+* Returns the hooray pased with the order inverted and modifies the given hooray
+*
+* @returns {Hooray} a new hooray with the order of the passed hooray inverted
+*/
+
+Hooray.prototype.reverse = function(){
+    var newHooray = new Hooray();
+
+    for(var i = this.length-1; i >= 0; i--) {
+        newHooray[newHooray.length] = this[i] ;
+        newHooray.length++
+    }
+    for(i = 0; i < newHooray.length; i++){
+        this[i] = newHooray[i];
+    }
+    return newHooray;
+}
+
+/**
+* deletes first element of the given hooray and returns it, this function modifies the hooray length
+*
+* @returns {any} the firs element of the given hooray.
+*/
+
+Hooray.prototype.shift = function(){
+    var newHooray = new Hooray();
+    var element;
+    for(var i = 1; i < this.length; i++){
+        newHooray[newHooray.length] = this[i];
+        newHooray.length++;
+    }
+    element = this[0];
+    this.length = newHooray.length;
+    delete(this[this.length-1]);
+
+    for(var j = 0; j < newHooray.length; j++){
+        this[j]=newHooray[j];
+    }
+    return element;
+}
+
+/**
+* returns a copy of a part of the given hooray, starting at begin and ending at end without including the end index element
+*
+* @param {number} begin
+* @param {number} end
+*
+* @returns {Hooray} new hooray with a part of the given hooray.
+*/
+
+
+Hooray.prototype.slice = function(begin, end){
+    var newHooray = new Hooray();
+    var i = 0;
+
+    if ((typeof end ==='number')||(end === undefined)){
+        if((begin !== undefined)&&(typeof begin == 'number')){
+            i = begin;
+        }else{i = 0;}
+
+        var z = (end === undefined) ? this.length : end;
+        for(i; i < z; i++){
+            newHooray[newHooray.length] = this[i];
+            newHooray.length++;
+        }
+    }
+    return newHooray;
+}
+
+/**
+* Tests if at least one element in the given hooray pases the test implemented in the provided function
+*
+* @param {any} value
+* @param {Function} callback
+*/
+
+Hooray.prototype.some = function(callback){
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+
+    for(var i = 0; i< this.length; i++) if (callback(this[i])) return true;
+
+    return false;
+}
+
+/**
+*
+* sorts the given hooray. Notice than numbers are considered smaller than letters
+*/
+
+Hooray.prototype.sort = function(){
+
+    for(var i= 0; i < this.length; i++){
+        var a = this[i];
+        var b = this[i+1];
+        if(this[i] > this[i+1] || (typeof(this[i+1])=='number')&&(typeof(this[i])=='string')){
+            this[i+1] = a;
+            this[i] = b;
+            i = -1;
+        }
+    }
+    return this;
+}
+
+/**
+ * puts element into the given hooray at the start position deleting until end number of positions
+ *
+ * @param {number} start the index of the hooray to start
+ * @param {number} end the index of the hooray to end
+ * @param {any} elements the element or elements to insert begining in the start position
+ */
+Hooray.prototype.splice = function(start, end, ...elements){
+    if ((typeof start !=='number') && (start !== undefined)) throw new TypeError(start + ' is not a number');
+    if ((typeof end !=='number') && (end !== undefined)) throw new TypeError(end + ' is not a number');
+
+    var startHooray = [];
+    var newargsHooray = [];
+    var endHooray = [];
+    var returnHooray = [];
+    var i = 0;
+
+    if ((start == undefined) || (start > this.length) || (arguments.length ==3) && (end == 0)){
+        return returnHooray;
+    }
+
+    if(start < 0) {
+        start = this.length + start;
+    }
+    if(!end || end >= this.length - start) {
+        end = this.length;
+    }
+    if(end < 0) end = start;
+
+    for(i = 0; i < start; i++) {
+        startHooray[startHooray.length] = this[i];
+        startHooray.length++
+    }
+
+    for(i = start; i < end; i++) {
+        returnHooray[returnHooray.length] = this[i];
+        returnHooray.length++
+    }
+
+    for(i = end; i < this.length; i++) {
+        endHooray[endHooray.length] = this[i];
+        endHooray.length++
+    }
+
+    if(arguments.length > 3) {
+        for(i = 3; i < arguments.length; i++) {
+            newargsHooray[newargsHooray.length] = arguments[i];
+            newargsHooray.length++
+        };
+    }
+    for(i = 0; i < this.length; i++){
+        delete(this[i]);
+    }
+    this.length = 0;
+    for ( i = 0; i < startHooray.length; i++){
+        this[this.length] = startHooray[i];
+        this.length++;
+    }
+    for(i = 0; i < newargsHooray.length; i++){
+        this[this.length] = newargsHooray[i];
+        this.length++;
+    }
+    for(i = 0; i < endHooray.length; i++){
+        this[this.length] = endHooray[i];
+        this.length++;
+    }
+    return returnHooray;
 }
--- staff/marc-uson/hooray/hooray.spec.js
+++ staff/marc-uson/hooray/hooray.spec.js
@@ -164,7 +164,7 @@ describe('hooray', function () {
         });
 
         true && it('should return false on any of the items not matching the condition', function () {
-            var hooray = [1, 2, 3];
+            var hooray =new Hooray(1, 2, 3);
 
             var result = hooray.every(function (v) { return v < 0; });
 
@@ -172,7 +172,7 @@ describe('hooray', function () {
         });
 
         true && it('should break on undefined callback', function () {
-            var hooray = [1, 2, 3];
+            var hooray = new Hooray(1, 2, 3);
 
             try {
                 hooray.every();
@@ -458,7 +458,7 @@ describe('hooray', function () {
         });
     });
 
-    describe('reduceRight', function(){
+    describe('reduce Right', function(){
         true && it('should return 29', function(){
             var hooray = new Hooray(1, 2, 5, 1, 20);
 
@@ -497,6 +497,253 @@ describe('hooray', function () {
                 expect(error.message).toBe('a is not a number');
             }
         });
+    });
+
+    describe('reduce', function(){
+        true && it('should return 29', function(){
+            var hooray = new Hooray(1, 2, 5, 1, 20);
+
+            var result = hooray.reduce(function(anterior, actual){ return anterior + actual});
+
+            expect(result).toBe(29);
+        });
+
+        true && it('should return abcde', function(){
+            var hooray = new Hooray('a', 'b', 'c', 'd', 'e');
+
+            var result = hooray.reduce(function(anterior, actual){ return anterior + actual});
+
+            expect(result).toBe('abcde');
+        });
+
+        true && it('should break on undefined is not a function', function(){
+            var hooray = new Hooray();
+
+            try {
+                hooray.reduce();
+
+                throw Error('should not reach this point');
+            } catch (error) {
+                expect(error.message).toBe('undefined is not a function');
+            }
+        });
+
+        true && it('should break on a is not a number', function(){
+            var hooray = new Hooray();
+
+            try {
+                hooray.reduce(function(){}, 'a');
+
+                throw Error('should not reach this point');
+            } catch (error) {
+                expect(error.message).toBe('a is not a number');
+            }
+        });
 
     });
+
+    describe('reverse', function(){
+        true && it('should return {20,1,5,2,1}', function(){
+            var hooray = new Hooray(1, 2, 5, 1, 20);
+
+            var result = hooray.reverse();
+
+            expect(result).toEqual(jasmine.objectContaining({0:20, 1:1, 2:5, 3:2, 4:1, length: 5}));
+            expect(hooray).toEqual(jasmine.objectContaining({0:20, 1:1, 2:5, 3:2, 4:1, length: 5}));
+        });
+
+        true && it('should return {e,d,c,b,a}', function(){
+            var hooray = new Hooray('a', 'b', 'c', 'd', 'e');
+
+            var result = hooray.reverse();
+
+            expect(result).toEqual(jasmine.objectContaining({0:'e',1:'d', 2:'c', 3:'b', 4:'a', length: 5}));
+            expect(hooray).toEqual(jasmine.objectContaining({0:'e',1:'d', 2:'c', 3:'b', 4:'a', length: 5}));
+        });
+    });
+
+    describe('shift', function(){
+        true && it('should return 1 and hooray modified {2,5,1,20}', function(){
+            var hooray = new Hooray(1, 2, 5, 1, 20);
+            var result = hooray.shift();
+
+            expect(hooray).toEqual(jasmine.objectContaining({0:2, 1:5, 2:1, 3:20, length:4}));
+            expect(result).toBe(1);
+        });
+
+        true && it('should return a and hooray modified {b,c,d,e}', function(){
+            var hooray = new Hooray('a', 'b', 'c', 'd', 'e');
+            var result = hooray.shift();
+
+            expect(hooray).toEqual(jasmine.objectContaining({0:'b', 1:'c', 2:'d', 3:'e', length:4}));
+            expect(result).toBe('a');
+        });
+    });
+
+    describe('slice', function(){
+        true && it('should return {5,1,20}', function(){
+            var hooray = new Hooray(1, 2, 5, 1, 20);
+
+            var result = hooray.slice(1);
+
+            expect(result).toEqual(jasmine.objectContaining({0:2, 1:5, 2:1, 3:20, length:4}));
+            expect(hooray).toEqual(jasmine.objectContaining({0:1, 1:2, 2:5, 3:1, 4:20, length:5}));
+        });
+
+        true && it('should return {b,c,d}', function(){
+            var hooray = new Hooray('a', 'b', 'c', 'd', 'e');
+
+            var result = hooray.slice(1,4);
+
+            expect(result).toEqual(jasmine.objectContaining({0:'b', 1:'c', 2:'d', length:3}));
+            expect(hooray).toEqual(jasmine.objectContaining({0:'a', 1:'b', 2:'c', 3:'d', 4:'e', length:5}));
+        });
+
+        true && it('should return {}', function(){
+            var hooray = new Hooray('a', 'b', 'c', 'd', 'e');
+
+            var result = hooray.slice(1,'a');
+
+            expect(result).toEqual(jasmine.objectContaining({length: 0}));
+            expect(hooray).toEqual(jasmine.objectContaining({0:'a', 1:'b', 2:'c', 3:'d', 4:'e', length:5}));
+        });
+
+        true && it('should return {1,2,5,1,20}', function(){
+            var hooray = new Hooray(1, 2, 5, 1, 20);
+
+            var result = hooray.slice('a');
+
+            expect(result).toEqual(jasmine.objectContaining({0:1, 1:2, 2:5, 3:1, 4:20, length:5}));
+            expect(hooray).toEqual(jasmine.objectContaining({0:1, 1:2, 2:5, 3:1, 4:20, length:5}));
+        });
+    });
+
+    describe('some', function(){
+        true && it('shoud return true cause some element in the hooray matches the function call', function(){
+            var hooray = new Hooray(1, 2, 5, 1, 20);
+
+            var result = hooray.some(function(element){return (element===5);});
+
+            expect(result).toBeTruthy();
+        });
+
+        true && it('shoud return false cause some element in the hooray matches the function call', function(){
+            var hooray = new Hooray('a', 'b', 'c', 'd', 'e');
+
+            var result = hooray.some(function(element){return (element===5);});
+
+            expect(result).toBeFalsy();
+        });
+
+        true && it('should break on undefined is not a function', function(){
+            var hooray = new Hooray();
+
+            try {
+                var result = hooray.some();
+
+                throw Error('should not reach this point');
+            } catch (error) {
+                expect(error.message).toBe('undefined is not a function');
+            }
+        });
+    });
+
+    describe('sort', function(){
+        true && it('should return the hooray modified {1, 1, 2, 5, 20}', function(){
+            var hooray = new Hooray(1, 2, 5, 1, 20);
+
+            hooray.sort();
+
+            expect(hooray).toEqual(jasmine.objectContaining({0:1, 1:1, 2:2, 3:5, 4:20, length:5}));
+        });
+
+        true && it('should return the hooray modified {1, 2, a, b, c, d}', function(){
+            var hooray = new Hooray('a', 'd', 2, 'c', 'b', 1);
+
+            hooray.sort();
+
+            expect(hooray).toEqual(jasmine.objectContaining({0:1, 1:2, 2:'a', 3:'b', 4:'c', 5:'d', length:6}));
+        });
+    });
+
+    describe('splice', function(){
+        true && it('should return an hooray modified {1, 2, 3,4,5,6,7,8,9} and {} as a result', function(){
+            var hooray = new Hooray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+            var result = hooray.splice();
+
+            expect(hooray).toEqual(jasmine.objectContaining({0:1, 1:2, 2:3, 3:4, 4:5, 5:6, 6:7, 7:8, 8:9, length:9}));
+            expect(result).toBeTruthy();
+
+        });
+        true && it('should return an hooray modified {1, 2, 3,4,5,6,7,8,9} and {} as a result', function(){
+            var hooray = new Hooray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+            var result = hooray.splice(20);
+
+            expect(hooray).toEqual(jasmine.objectContaining({0:1, 1:2, 2:3, 3:4, 4:5, 5:6, 6:7, 7:8, 8:9, length:9}));
+            expect(result).toBeTruthy();
+
+        });
+        true && it('should return an hooray modified {1,2,3,4,5,6,7,8,9} and {} as a result', function(){
+            var hooray = new Hooray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+            var result = hooray.splice(1, 0);
+
+            expect(hooray).toEqual(jasmine.objectContaining({0:1, 1:2, 2:3, 3:4, 4:5, 5:6, 6:7, 7:8, 8:9, length:9}));
+            expect(result).toBeTruthy();
+
+        });
+        true && it('should return an hooray modified {1, 2, 3} and {4,5,6,7,8,9} as a result', function(){
+            var hooray = new Hooray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+            var result = hooray.splice(3);
+
+            expect(hooray).toEqual(jasmine.objectContaining({0:1, 1:2, 2:3, length:3}));
+            expect(result).toEqual(jasmine.objectContaining({0:4, 1:5, 2:6, 3:7, 4:8, 5:9, length: 6}));
+
+        });
+        true && it('should return an hooray modified {1, a, 2, 3, 4, 5, 6, 7, 8, 9} and {} as a result', function(){
+            var hooray = new Hooray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+            var result = hooray.splice(1, 2, 'a');
+
+            expect(hooray).toEqual(jasmine.objectContaining({0:1, 1:'a', 2:3, 3:4, 4:5, 5:6, 6:7, 7:8, 8:9, length:9}));
+            expect(result).toEqual(jasmine.objectContaining({0:2, length:1}));
+        });
+
+        true && it('should return an hooray modified {a,b,1,2,3}', function(){
+            var hooray = new Hooray('a', 'b', 'c', 'd', 'e');
+
+            var result = hooray.splice(2, 3, 1, 2, 3);
+
+            expect(hooray).toEqual(jasmine.objectContaining({0:'a', 1:'b', 2:1, 3:2, 4:3, length:5}));
+            expect(result).toEqual(jasmine.objectContaining({0:'c', 1:'d', 2:'e', length:3}));
+        });
+
+        true && it('should break on a is not  number', function(){
+            var hooray = new Hooray();
+
+            try {
+                hooray.splice('a');
+
+                throw Error('should not reach this point');
+            } catch (error) {
+                expect(error.message).toBe('a is not a number');
+            }
+        });
+
+        true && it('should break on a is not  number', function(){
+            var hooray = new Hooray();
+
+            try {
+                hooray.splice(0, 'a');
+
+                throw Error('should not reach this point');
+            } catch (error) {
+                expect(error.message).toBe('a is not a number');
+            }
+        });
+    });
+
 });
