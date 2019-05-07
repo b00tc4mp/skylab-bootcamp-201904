f0947e0f24bd2fe83d8ede684a7b481266cffbaf
Markuson <el.marcuson@gmail.com>
2019-05-07 19:21:03 +0200

    done recive TODO send

---
+++ staff/marc-uson/chat/send.js
@@ -0,0 +1,36 @@
+const http = require('http')
+
+const{argv: [, , host, port, message]} = process
+
+
+
+
+const data = JSON.stringify({
+  todo: 'Buy the milk'
+})
+
+const options = {
+  hostname: 'flaviocopes.com',
+  port: 443,
+  path: '/todos',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', (error) => {
+  console.error(error)
+})
+
+req.write(data)
+req.end()
