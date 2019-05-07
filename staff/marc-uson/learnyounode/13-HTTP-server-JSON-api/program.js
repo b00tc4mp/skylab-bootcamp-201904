1435922953d5689e1c9631bbdb59f32de09d4b16
Markuson <el.marcuson@gmail.com>
2019-05-07 17:47:57 +0200

    finishing learnyounode with errors in 13

--- staff/marc-uson/learnyounode/13-HTTP-server-JSON-api/program.js
+++ staff/marc-uson/learnyounode/13-HTTP-server-JSON-api/program.js
@@ -1,16 +1,42 @@
-var http = require('http')
-var map = require('through2-map')
+const http = require('http')
+const url = require('url')
 
-const {argv:[, ,port]} = process
+const {argv: [ , ,port]} = process
 
-http.createServer(function (request, response) {
-    if(request.method === 'GET'){
+const server = http.createServer((req, res) => {
+  switch (url.parse(req.url).pathname){
 
-      request.pipe(map(function (chunk) {
-        return chunk.toString().toUpperCase()
-      })).pipe(response)
+    case '/api/parsetime':
 
-    }
+      const date = new Date(url.parse(req.url, true).iso)
 
-}).listen(port)
+      resp = {
+        "hour": date.getHours(),
+        "minute": date.getMinutes(),
+        "second": date.getSeconds()
+      }
 
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(resp))
+      break
+
+    case '/api/unixtime':
+
+      const date2 = new Date(url.parse(req.url, true).iso)
+
+      const resp = {
+        unixtime: date2.getTime()
+      }
+
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(resp))
+      break
+
+    default:
+    res.end('Incorrect path')
+
+  }
+
+})
+
+server.listen(port)
